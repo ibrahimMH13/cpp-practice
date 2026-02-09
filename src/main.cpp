23a0fc6b6ca345bd97a74d4a1ebb4347b49b7e98
Ibrahim II Musabeh <i.musabah92@gmail.com>
2026-02-09 19:54:59 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include <mutex>
 #include <condition_variable>
 #include <thread>
+#include <deque>
+#include <chrono>
 
 struct Task
 {
@@ -115,6 +117,59 @@ private:
     std::condition_variable cv_;
 };
 
+template <typename T>
+class BoundedQueue
+{
+
+public:
+    explicit BoundedQueue(std::size_t capacity) : capacity_(capacity) {};
+    bool push(T item)
+    {
+        std::unique_lock<std::mutex> lock(mtx_);
+        cv_not_full_.wait(lock, [&]
+                          { return shutdown_ || q_.size() < capacity_; });
+        if (shutdown_)
+            return false;
+        q_.push_back(std::move(item));
+        cv_not_empty_.notify_one();
+        return true;
+    }
+    std::optional<T> pop()
+    {
+        std::unique_lock<std::mutex> lock(mtx_);
+        cv_not_empty_.wait(lock, [&]
+                           { return shutdown_ || !q_.empty(); });
+
+        if (q_.empty())
+        {
+            return std::nullopt;
+        }
+
+        T item = std::move(q_.front());
+        q_.pop_front();
+        cv_not_full_.notify_one();
+        return item;
+    }
+    void shutdown()
+    {
+        {
+            std::unique_lock<std::mutex> lock(mtx_);
+            shutdown_ = true;
+        }
+        cv_not_empty_.notify_all();
+        cv_not_full_.notify_all();
+    }
+
+private:
+    std::size_t capacity_;
+    std::mutex mtx_;
+    bool shutdown_ = false;
+    std::condition_variable cv_not_full_;
+    std::condition_variable cv_not_empty_;
+    std::deque<T> q_;
+};
+
+
 int main()
 {
 
@@ -138,41 +193,40 @@ int main()
 
     taskScheduler.cancel("b");
 
-  // taskScheduler.tryGetNext();
-  for(int i = 0;i < workerCount;++i){
-    workers.emplace_back([&taskScheduler, i]{
-            while (auto t = taskScheduler.getNext())
+    // taskScheduler.tryGetNext();
+    for (int i = 0; i < workerCount; ++i)
     {
-                std::cout       << "[Worker=" << i << "] "
-                                 << "task=" << t->task_id << " "
-                                 <<"priority="    << t->priority << " "
-                                 << "ts="   << t->ts << " \n";
-                std::cout << "[worker " << i << "] exiting\n";
-                 
+        workers.emplace_back([&taskScheduler, i]
+                             {
+                                 while (auto t = taskScheduler.getNext())
+                                 {
+                                     std::cout << "[Worker=" << i << "] "
+                                               << "task=" << t->task_id << " "
+                                               << "priority=" << t->priority << " "
+                                               << "ts=" << t->ts << " \n";
+                                     std::cout << "[worker " << i << "] exiting\n";
+                                 }
+                             });
     }
-
-
-    });
-  }
-        taskScheduler.submit({"a", 102, 24});
-        taskScheduler.submit({"b", 102, 24});
-        taskScheduler.submit({"c", 100, 242});
-        taskScheduler.submit({"d", 101, 241});
-        taskScheduler.cancel("b");
-            // Add more tasks later to see concurrency
+    taskScheduler.submit({"a", 102, 24});
+    taskScheduler.submit({"b", 102, 24});
+    taskScheduler.submit({"c", 100, 242});
+    taskScheduler.submit({"d", 101, 241});
+    taskScheduler.cancel("b");
+    // Add more tasks later to see concurrency
     for (int k = 0; k < 6; ++k)
     {
-        taskScheduler.submit({ "x" + std::to_string(k), 100 + (k % 3), 10 + (std::uint64_t)k });
+        taskScheduler.submit({"x" + std::to_string(k), 100 + (k % 3), 10 + (std::uint64_t)k});
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
     // Give workers time to drain queue (for demo only)
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-//    // Stop workers and join
-  //  taskScheduler.shutdown();
-    for (auto& th : workers) th.join();
-
+    //    // Stop workers and join
+    //  taskScheduler.shutdown();
+    for (auto &th : workers)
+        th.join();
 
     return 0;
 }
