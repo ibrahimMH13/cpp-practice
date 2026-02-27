d51312632222348dc7cd4e7621bcf9e05a47941b
Ibrahim II Musabeh <i.musabah92@gmail.com>
2026-02-27 22:19:34 +0100

    practice

--- src/KClosest.cpp
+++ src/KClosest.cpp
@@ -27,6 +27,27 @@ public:
         }
         return result;
     }
+
+    vector<vector<int>> KClose(vector<vector<int>>& points, int k){
+
+        priority_queue<pair<int, vector<int>>> maxHeap;
+        for (auto p: points)
+        {
+           int dist = p[0] * p[0] + p[1] * p[1];
+           maxHeap.push({dist,p});
+           if (maxHeap.size() > k)
+           {
+                maxHeap.pop();
+           }
+        }
+        vector<vector<int>> result;
+        while (!maxHeap.empty())
+        {   
+              result.push_back(maxHeap.top().second);
+                maxHeap.pop();
+        }
+        return result;
+    }
 };
 
 int main()
